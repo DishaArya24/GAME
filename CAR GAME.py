21c1f04611a8d0f37a171dbf2bc6042f6e47eecd
Disha.H <dishaarya389@gmail.com>
2024-07-06 21:06:03 +0530

    Add files via upload

---
+++ CAR GAME.py
@@ -0,0 +1,29 @@
+command = ""
+started = False
+while True:
+
+    command = input(">").lower()
+    if command == "start":
+        if started:
+             print("Car is already  started!")
+        else:
+            started = True
+            print("car is started....")
+    elif command  == "stop" :
+         if not started:
+             print("Car is already stopped!")
+         else:
+             started = False
+             print("Car stopped.")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - stop the car
+quit - to quit the game
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("I DONT GET YOU")
+
+
